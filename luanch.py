6f702ba3959c6854901adf348c35ca938641f66d
승빈 오 <sboh1214@gmail.com>
2018-08-01 14:19:06 +0900

    실행파일 제작중

--- luanch.py
+++ luanch.py
@@ -1,7 +1,13 @@
 import os
 
+print("====================")
+print("Auto Launch\n")
+print("ex) Path : ")
+print("====================")
+
+path = input("Path : ")
 red = input("Red Team : ")
 blue = input("Blue Team : ")
-os.system("cd src")
+os.system("cd "+path)
 os.system("capture.py -r "+str(red)+" -b "+str(blue))
 os.system("cd ..")
